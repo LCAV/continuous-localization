a1322268c5f78e4a35d3bd3e651aee02d0829820
Frederike <frederike.duembgen@epfl.ch>
2018-07-12 14:52:31 +0200

    Made older MDS code work again.

--- trajectory.py
+++ trajectory.py
@@ -14,6 +14,7 @@ from global_variables import DIM
 trajectory.py: 
 """
 
+
 class Trajectory(object):
     def __init__(self, n_positions=8, n_complexity=3):
         self.n_positions = n_positions
@@ -29,18 +30,24 @@ class Trajectory(object):
                                    n_samples), [1, n_samples])
         return np.cos(np.pi * k * n / self.n_positions)
 
-
     def set_basis(self, n_samples=None):
         """ Sets self.basis. """
         self.basis = self.get_basis(n_samples)
 
-    def set_random_trajectory(self, seed=None):
-        """ Sets self.coeffs, self.Z_opt, self.trajectory. """
+    def set_trajectory(self, seed=None, coeffs=None):
+        """ Sets self.coeffs, self.Z_opt, self.trajectory. 
+        
+        :param coeffs: Matrix of coefficients, DIM X n_complexity. If None, random coeffs are created. 
+        :param seed: random seed.
+        """
         if seed is not None:
             np.random.seed(seed)
 
-        self.coeffs = 5 * \
-            np.random.rand(DIM, self.n_complexity)
+        if coeffs is None:
+            self.coeffs = 5 * \
+                np.random.rand(DIM, self.n_complexity)
+        else:
+            self.coeffs = coeffs
 
         self.Z_opt = np.vstack([np.hstack([np.eye(DIM), self.coeffs]),
                                 np.hstack([self.coeffs.T, self.coeffs.T @ self.coeffs])])
@@ -50,9 +57,34 @@ class Trajectory(object):
 
         return self.trajectory
 
-    def plot(self):
+    def plot(self, mask=None, **kwargs):
         basis_cont = self.get_basis(n_samples=1000)
         trajectory_cont = self.coeffs @ basis_cont
 
-        plt.scatter(*self.trajectory, color='orange')
-        plt.plot(*trajectory_cont, color='orange')
+        if mask is not None:
+            trajectory = self.trajectory[:, np.any(
+                mask[:self.n_positions, :] != 0, axis=1)]
+        else:
+            trajectory = self.trajectory
+
+        plt.scatter(*self.trajectory, **kwargs)
+        plt.plot(*trajectory_cont, **kwargs)
+
+    def plot_number_measurements(self, mask=None, **kwargs):
+        basis_cont = self.get_basis(n_samples=1000)
+        trajectory_cont = self.coeffs @ basis_cont
+
+        plt.plot(*trajectory_cont)
+        for i in range(self.n_positions):
+            point = self.trajectory[:, i]
+            if np.sum(mask[i, :]) == 1:
+                plt.scatter(*point, color='orange')
+            if np.sum(mask[i, :]) == 2:
+                plt.scatter(*point, color='red')
+            if np.sum(mask[i, :]) > 2:
+                plt.scatter(*point, color='green')
+
+        plt.scatter(-2.8, 6)
+        plt.scatter(-2.8, 5, color='orange')
+        plt.scatter(-2.8, 4, color='red')
+        plt.scatter(-2.8, 3, color='green')
