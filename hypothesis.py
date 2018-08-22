8bac5714c208db2a150cbe82985f7ed89a43786e
Michalina Pacholska <micha7a@gmail.com>
2018-08-22 17:35:36 +0200

    Clean up filenames in rank testing.
    All files will be saved in the same folder, but the key will depend
    on the `fixed_probability` variable which is true if the probability
    is fixed and the number of measurements can vary in each repetition.

--- hypothesis.py
+++ hypothesis.py
@@ -1,6 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.ticker as tck
 import os
 
 
@@ -48,65 +46,36 @@ if __name__ == '__main__':
 
     n_dimensions = 2
     n_constrains = 5
-    n_anchors = 3
-    n_positions = 50
-    n_repetitions = 1000
-    directory = "results/ranks_prob/"
+    n_anchors = 10
+    n_positions = 30
+    n_repetitions = 10
+    fixed_probability = True
+    directory = "results/ranks/"
     if not os.path.exists(directory):
         os.makedirs(directory)
-    ratio = n_dimensions * n_constrains / (n_dimensions * n_positions)
-    max_prob = min(2 * ratio, 1)
-    probabilities = np.linspace(0, max_prob, 100)
 
-    key = "_d{}_c{}_a{}_p{}".format(n_dimensions, n_constrains, n_anchors, n_positions)
+    probabilities = np.linspace(0, 1, 100)
 
-    ranks = np.zeros((len(probabilities), n_repetitions))
-    # edges = np.zeros_like(ranks)
-    # v_anchors = np.zeros_like(ranks)
-    # v_positions = np.zeros_like(ranks)
+    key = "_d{}_c{}_a{}_p{}_{}".format(n_dimensions, n_constrains, n_anchors, n_positions,
+                                       fixed_probability)
+
     anchors = get_anchors(n_anchors, n_dimensions)
     frame = get_frame(n_constrains, n_positions)
-    # for n_measurements in range(2, n_positions * n_anchors + 1):
-    for idx, prob in enumerate(probabilities):
-        for r in range(n_repetitions):
-            matrix = np.random.binomial(n=1, p=prob, size=(n_anchors, n_positions))
-            idx_a, idx_f = matrix_to_indexes(matrix)
-            constrains = get_upper_right_constrains(idx_a, idx_f, anchors, frame)
-            ranks[idx, r] = np.linalg.matrix_rank(constrains)
-            # edges[idx, r] = np.sum(np.sum(matrix))
-            # v_anchors[idx, r] = np.sum(np.sum(matrix, 1) >= n_constrains)
-            # v_positions[idx, r] = np.sum(np.sum(matrix, 0) >= n_dimensions)
+
+    if fixed_probability:
+        ranks = np.zeros((len(probabilities), n_repetitions))
+        for idx, prob in enumerate(probabilities):
+            for r in range(n_repetitions):
+                matrix = np.random.binomial(n=1, p=prob, size=(n_anchors, n_positions))
+                idx_a, idx_f = matrix_to_indexes(matrix)
+                constrains = get_upper_right_constrains(idx_a, idx_f, anchors, frame)
+                ranks[idx, r] = np.linalg.matrix_rank(constrains)
+    else:
+        ranks = np.zeros((n_positions * n_anchors + 1, n_repetitions))
+        for n_measurements in range(2, n_positions * n_anchors + 1):
+            for r in range(n_repetitions):
+                idx_a, idx_f = random_indexes(n_anchors, n_positions, n_measurements)
+                constrains = get_upper_right_constrains(idx_a, idx_f, anchors, frame)
+                ranks[n_measurements, r] = np.linalg.matrix_rank(constrains)
 
     np.save(directory + "ranks" + key, ranks)
-    # np.save(directory + "anchors_degree" + key, v_anchors)
-    # np.save(directory + "positions_degree" + key, v_positions)
-
-    # DK_multiples = (np.arange(n_positions * n_anchors) + 1) / (n_dimensions * n_constrains)
-    f, ax = plt.subplots()
-    plt.plot(
-        probabilities / ratio,
-        np.mean(ranks, axis=1) / (n_dimensions * n_constrains),
-        label="mean rank / full rank")
-    plt.plot(
-        probabilities / ratio,
-        np.sum(ranks >= n_dimensions * n_constrains, axis=1) / n_repetitions,
-        label="prob. of full rank")
-    # plt.plot(probabilities / ratio,
-    #     np.mean(edges, axis=1) / (n_anchors * n_positions))
-    plt.xlabel("alpha")
-    plt.grid()
-    # ax.xaxis.set_major_formatter(tck.FormatStrFormatter('%g DK'))
-    # ax.xaxis.set_major_locator(tck.MultipleLocator(base=1))
-    plt.legend()
-    plt.savefig(directory + "ranks_DK" + key + ".pdf")
-    plt.show()
-
-    # f, ax = plt.subplots()
-    # plt.plot(DK_multiples, np.mean(v_anchors/n_dimensions, axis=1), label="# highly connected anchors")
-    # plt.plot(DK_multiples, np.mean(v_positions/n_constrains, axis=1), label="# highly connected positions")
-    # plt.xlabel("number of measurements")
-    # plt.grid()
-    # ax.xaxis.set_major_formatter(tck.FormatStrFormatter('%g DK'))
-    # ax.xaxis.set_major_locator(tck.MultipleLocator(base=1))
-    # plt.legend()
-    # plt.show()
