1a12fd1856225e3d61b2a0ce0c0fa0f1159b75d2
Frederike <frederike.duembgen@epfl.ch>
2019-03-04 14:51:02 +0100

    Added new trajectory models.

--- trajectory.py
+++ trajectory.py
@@ -7,27 +7,44 @@ trajectory.py: Contains the Trajectory class.
 import numpy as np
 import matplotlib.pyplot as plt
 
-from global_variables import DIM, TMAX
+from global_variables import DIM, TMAX, TAU
 
 
 class Trajectory(object):
     """ Trajectory class.
 
     :member n_complexity: complexity of trajectory. 
+    :member n_positions: Number of positions where we measure. 
     :member coeffs: coefficients of trajectory. (dim x n_complexity)
     :member trajectory: list of points of trajectory (dim x n_positions)
     :member basis: basis vectors evaluated at different time instances. (n_complexity x n_positions)
+    :member model: trajectory model, either bandlimited or polynomial. 
     """
 
     # TODO maybe remove n_positions, trajectory and basis from this class.
     # It should be possible to generate them using an independent vector
     # of "times", of length n_positions.
-    def __init__(self, n_positions=8, n_complexity=3, dim=DIM):
+    def __init__(self, n_positions=8, n_complexity=3, dim=DIM, model='bandlimited', tau=TAU):
         self.n_positions = n_positions
         self.n_complexity = n_complexity
         self.coeffs = np.empty((dim, n_complexity))
         self.trajectory = np.empty((dim, n_positions))
         self.basis = np.empty((n_complexity, n_positions))
+        self.model = model
+        if model == 'bandlimited':
+            assert tau is not None, 'Need to specify period tau when creating bandlimited trajectory.'
+        self.params = {'tau': tau}
+
+    def get_times(self, n_samples):
+        """ Get times appropriate for this trajectory model. """
+        if self.model == 'polynomial':
+            times = np.linspace(0, TMAX, n_samples)
+        elif self.model == 'bandlimited':
+            times = np.linspace(0, self.params['tau'] / 2.0, n_samples)
+        else:
+            raise NotImplementedError(self.model)
+
+        return times
 
     def get_basis(self, n_samples=None, times=None):
         """ Get basis vectors evaluated at specific times. 
@@ -39,11 +56,14 @@ class Trajectory(object):
             n_samples = self.n_positions
 
         if times is None:
-            times = np.linspace(0, TMAX, n_samples)
+            times = self.get_times(n_samples)
 
         k = np.reshape(range(self.n_complexity), [self.n_complexity, 1])
         n = np.reshape(times, [1, n_samples])
-        return np.cos(np.pi * k * n / n_samples)
+        if self.model == 'bandlimited':
+            return np.cos(2 * np.pi * k * n / self.params['tau'])
+        elif self.model == 'polynomial':
+            return np.power(n, k)
 
     def set_basis(self, n_samples=None, times=None):
         """ Sets self.basis. """
@@ -57,7 +77,7 @@ class Trajectory(object):
             self.coeffs = 5 * \
                 np.random.rand(*self.coeffs.shape)
         else:
-            assert all(coeffs.shape == self.coeffs.shape)
+            np.testing.assert_equal(coeffs.shape, self.coeffs.shape)
             self.coeffs = coeffs
 
         dim = self.coeffs.shape[0]
@@ -72,7 +92,6 @@ class Trajectory(object):
         :param coeffs: Matrix of coefficients, dim X n_complexity. If None, random coeffs are created. 
         :param seed: random seed.
         """
-
         self.set_coeffs(seed, coeffs)
         self.set_basis(times=times)
         self.trajectory = self.coeffs @ self.basis
