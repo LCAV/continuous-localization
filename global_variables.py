079fc2de1d1dd695431ecf96777eeff0da77338e
Frederike <frederike.duembgen@epfl.ch>
2019-03-04 14:49:14 +0100

    Added documentation.

--- global_variables.py
+++ global_variables.py
@@ -1,11 +1,9 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
-#
-# Copyright © 2018 Frederike Duembgen <frederike.duembgen@gmail.com>
-#
-# Distributed under terms of the MIT license.
 """
-global_variables.py: 
+global_variables.py: Some default variable values of project. 
 """
-DIM = 2
-TMAX = 2.0
+
+DIM = 2  #: dimension of setup.
+TMAX = 1.0  #: for polynomial trajectories: duration of trajectory in seconds. .
+TAU = 2.0  #: for bandlimited trajectories: bandwith in seconds.
