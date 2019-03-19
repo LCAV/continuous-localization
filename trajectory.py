b47f3800046c307edf7f179b6fc36082bee15be6
Frederike <frederike.duembgen@epfl.ch>
2019-03-19 14:08:44 +0100

    Introduced measurements module.

--- trajectory.py
+++ trajectory.py
@@ -13,23 +13,16 @@ from global_variables import DIM, TMAX, TAU
 class Trajectory(object):
     """ Trajectory class.
 
+    :member dim: dimension (2 or 3)
     :member n_complexity: complexity of trajectory. 
-    :member n_positions: Number of positions where we measure. 
     :member coeffs: coefficients of trajectory. (dim x n_complexity)
-    :member trajectory: list of points of trajectory (dim x n_positions)
-    :member basis: basis vectors evaluated at different time instances. (n_complexity x n_positions)
     :member model: trajectory model, either bandlimited or polynomial. 
     """
 
-    # TODO maybe remove n_positions, trajectory and basis from this class.
-    # It should be possible to generate them using an independent vector
-    # of "times", of length n_positions.
-    def __init__(self, n_positions=8, n_complexity=3, dim=DIM, model='bandlimited', tau=TAU):
-        self.n_positions = n_positions
+    def __init__(self, n_complexity=3, dim=DIM, model='bandlimited', tau=TAU):
+        self.dim = dim
         self.n_complexity = n_complexity
-        self.coeffs = np.empty((dim, n_complexity))
-        self.trajectory = np.empty((dim, n_positions))
-        self.basis = np.empty((n_complexity, n_positions))
+        self.coeffs = None
         self.model = model
         if model == 'bandlimited':
             assert tau is not None, 'Need to specify period tau when creating bandlimited trajectory.'
@@ -49,14 +42,21 @@ class Trajectory(object):
     def get_basis(self, n_samples=None, times=None):
         """ Get basis vectors evaluated at specific times. 
 
-        :param times: vector of times. 
-        :return: basis vector matrix (n_complexity x n_positions)
-        """
-        if n_samples is None:
-            n_samples = self.n_positions
+        :param n_samples: number of samples. 
+        :param times: vector of times of length n_samples
 
-        if times is None:
+        :return: basis vector matrix (n_complexity x n_samples)
+        """
+        if n_samples is None and times is None:
+            raise NotImplementedError('Need to give times or n_samples.')
+        elif times is None:
             times = self.get_times(n_samples)
+        elif times is not None and n_samples is not None:
+            raise AttributeError('Cannot give n_samples and times.')
+        elif times is not None and n_samples is None:
+            n_samples = len(times)
+        else:
+            raise NotImplementedError('case not treated:', n_samples, times)
 
         k = np.reshape(range(self.n_complexity), [self.n_complexity, 1])
         n = np.reshape(times, [1, n_samples])
@@ -65,58 +65,59 @@ class Trajectory(object):
         elif self.model == 'polynomial':
             return np.power(n, k)
 
-    def set_basis(self, n_samples=None, times=None):
-        """ Sets self.basis. """
-        self.basis = self.get_basis(n_samples, times)
-
     def set_coeffs(self, seed=None, coeffs=None):
         if seed is not None:
             np.random.seed(seed)
 
         if coeffs is None:
             self.coeffs = 5 * \
-                np.random.rand(*self.coeffs.shape)
+                np.random.rand(self.dim, self.n_complexity)
         else:
-            np.testing.assert_equal(coeffs.shape, self.coeffs.shape)
             self.coeffs = coeffs
 
         dim = self.coeffs.shape[0]
-        self.Z_opt = np.vstack([
-            np.hstack([np.eye(dim), self.coeffs]),
-            np.hstack([self.coeffs.T, self.coeffs.T @ self.coeffs])
-        ])
+        self.Z_opt = np.vstack(
+            [np.hstack([np.eye(dim), self.coeffs]),
+             np.hstack([self.coeffs.T, self.coeffs.T @ self.coeffs])])
 
-    def set_trajectory(self, seed=None, coeffs=None, times=None):
-        """ Sets self.coeffs, self.Z_opt, self.trajectory. 
+    def get_sampling_points(self, basis=None, seed=None):
+        """ Get points where we get measurements.
         
-        :param coeffs: Matrix of coefficients, dim X n_complexity. If None, random coeffs are created. 
-        :param seed: random seed.
         """
-        self.set_coeffs(seed, coeffs)
-        self.set_basis(times=times)
-        self.trajectory = self.coeffs @ self.basis
+        points = self.coeffs @ basis
+        return points
 
-        return self.trajectory
+    def get_continuous_points(self):
+        times_cont = self.get_times(n_samples=1000)
+        trajectory_cont = self.get_sampling_points(times=times_cont)
+        return trajectory_cont
 
-    def plot(self, mask=None, **kwargs):
-        basis_cont = self.get_basis(n_samples=1000)
-        trajectory_cont = self.coeffs @ basis_cont
+    def plot(self, times, mask=None, **kwargs):
+        """ Plot continuous and sampled version.
+
+        :param times: times of sampling points.
+        :param mask: optional measurements mask (to plot missing measurements)
+        :param kwargs: any additional kwargs passed to plt.scatter()
+
+        """
+
+        trajectory_cont = self.get_continuous_points()
+        trajectory = self.get_sampling_points(times=times)
 
         if mask is not None:
-            trajectory = self.trajectory[:, np.any(mask[:self.n_positions, :] != 0, axis=1)]
-        else:
-            trajectory = self.trajectory
+            trajectory = trajectory[:, np.any(mask[:self.n_positions, :] != 0, axis=1)]
 
-        plt.scatter(*self.trajectory, **kwargs)
-        plt.plot(*trajectory_cont, **kwargs)
+        plt.plot(*trajectory_cont, color='blue')
+        plt.scatter(*trajectory, **kwargs)
 
-    def plot_number_measurements(self, mask=None, **kwargs):
-        basis_cont = self.get_basis(n_samples=1000)
-        trajectory_cont = self.coeffs @ basis_cont
+    def plot_number_measurements(self, times, mask=None, **kwargs):
+        trajectory_cont = self.get_continuous_points()
+        trajectory = self.get_sampling_points(times=times)
 
-        plt.plot(*trajectory_cont)
-        for i in range(self.n_positions):
-            point = self.trajectory[:, i]
+        plt.plot(*trajectory_cont, color='blue')
+
+        for i in range(len(times)):
+            point = trajectory[:, i]
             if np.sum(mask[i, :]) == 1:
                 plt.scatter(*point, color='orange')
             if np.sum(mask[i, :]) == 2:
