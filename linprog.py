03969e5505fe8f45bfaf137c63d033ca2a6b19fe
Frederike <frederike.duembgen@epfl.ch>
2019-03-30 17:43:01 +0100

    Added more documentation.

--- linprog.py
+++ linprog.py
@@ -1,34 +1,54 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-linprog.py: Linear program to find values of a, b, c  
+linprog.py: Linear program to empirically find values of a, b, c. 
+
+a is how much a constraint going from the whole plain to a circle is worth. 
+b is how much a constraint going from a circle to two points is worth. 
+c is how much a constrint going from two points to a single point is worth. 
+
 """
 
 import numpy as np
 from scipy.optimize import linprog
 
+eps = 1e-3  #0.5
+c = 1
+
 
 def attempt_one():
+    """ In this attempt, I am only adding constraints for successful reconstructions. """
 
     A = [
+        [-3, -3, 1],  # success
         [-4, -3, 1],  # success
         [-5, -2, 1],  # success
         [-6, -1, 1],  # success
-        [-3, -3, 1],  # success
         [5, 2, -1],  # fail 
         [3, 3, -1],  # fail 
         [-1, 1, 0],  # a > b
         [0, -1, 0],  # b > c
-        [0, 0, -1],  # K > K_min
     ]  # require -4*x + -3*y <= -K + 2c etc.
-    b = [2 * c, c, 0, 3 * c, 0, 2 * c, 0, -c, -K_min]
+    b = [
+        3 * c,  # success
+        2 * c,  # success
+        c,  # success
+        0,  # success
+        0,  # fail 
+        2 * c,  # fail
+        0,
+        -c
+    ]
 
     f = [0, 0, 1]  # minimize K
     return A, b, f
 
 
 def attempt_two():
-    eps = 0.5  #0.5
+    """ In this attempt, I am adding constraints for successful reconstructions
+    and the corresponding constraints form failures ('if I remove any measurement, 
+    then the reconstruction will fail.. 
+    """
 
     A = [
         [-3, -3, 1],  # success
@@ -39,9 +59,9 @@ def attempt_two():
         [-5, -2, 1],  # success
         [5, 2, -1],  # fail
         [5, 1, -1],  # fail
+        [-6, -1, 1],  # success
         [-1, 1, 0],  # a > b
         [0, -1, 0],  # b > c
-        [0, 0, -1]  # K > K_min
     ]
     b = [
         3 * c,  # success
@@ -52,9 +72,9 @@ def attempt_two():
         c,  # success
         -eps,  # fail
         -c - eps,  # fail
-        0,
-        -c,
-        -K_min
+        0,  # success
+        -eps,
+        -c - eps,
     ]
 
     f = [0, 0, 1]  # minimize K
@@ -62,24 +82,30 @@ def attempt_two():
 
 
 if __name__ == "__main__":
-    c = 1
-    K = 10
+    import sys
 
     n_complexity = 3
     dim = 2
     K_min = n_complexity * dim
-    print(K_min)
+    print('minimum K:', K_min)
 
     #A, b, f = attempt_one()
-    A, b, f = attempt_two()
+    A, b_vec, f = attempt_two()
 
-    res = linprog(f, A_ub=A, b_ub=b)
-    print(res)
+    equality_matrix = [[1, 1, 0]]  # a + b = 2
+    equality_vector = [2]
+
+    #res = linprog(f, A_ub=A, b_ub=b_vec, A_eq=equality_matrix,
+    #              b_eq=equality_vector)
+    res = linprog(f, A_ub=A, b_ub=b_vec)
+    if not res.success:
+        print('no solution found.')
+        sys.exit
 
     a, b, K = res.x
-    print('a={}, b={}'.format(a, b))
+    print('a={:2.2f}, b={:2.2f}, K={:2.2f}'.format(a, b, K))
 
     A = np.array(A)
     x = np.array(res.x)
-    b = np.array(b)
-    print(A.dot(x) <= b)
+    b_vec = np.array(b_vec)
+    print('constraints satisfied: should all be negative \n', A.dot(x) - b_vec)
