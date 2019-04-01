1f266c440ca37cada9b5a7e9c3b085581cea8112
Frederike <frederike.duembgen@epfl.ch>
2019-04-01 16:25:27 +0200

    Finished linear programming solution.

--- linprog.py
+++ linprog.py
@@ -12,74 +12,49 @@ c is how much a constrint going from two points to a single point is worth.
 import numpy as np
 from scipy.optimize import linprog
 
-eps = 1e-3  #0.5
-c = 0.5
 
-
-def attempt_one():
-    """ In this attempt, I am only adding constraints for successful reconstructions. """
-
-    A = [
-        [-3, -3, 1],  # success
-        [-4, -3, 1],  # success
-        [-5, -2, 1],  # success
-        [-6, -1, 1],  # success
-        [5, 2, -1],  # fail 
-        [3, 3, -1],  # fail 
-        [-1, 1, 0],  # a > b
-        [0, -1, 0],  # b > c
-    ]  # require -4*x + -3*y <= -K + 2c etc.
-    b = [
-        3 * c,  # success
-        2 * c,  # success
-        c,  # success
-        0,  # success
-        0,  # fail 
-        2 * c,  # fail
-        0,
-        -c
-    ]
-
-    f = [0, 0, 1]  # minimize K
-    return A, b, f
-
-
-def attempt_two():
+def setup_constraints(c, eps):
     """ In this attempt, I am adding constraints for successful reconstructions
     and the corresponding constraints form failures ('if I remove any measurement, 
     then the reconstruction will fail.. 
     """
 
-    A = [
-        [-3, -3, 1],  # success
-        [3, 3, -1],  # fail
-        [-4, -3, 1],  # success
-        [4, 2, -1],  # fail
-        [4, 3, -1],  # fail
-        [-5, -2, 1],  # success
-        [5, 2, -1],  # fail
-        [5, 1, -1],  # fail
-        [-6, -1, 1],  # success
-        [-3, -4, 1],  # success
-        [-1, 1, 0],  # a > b
-        [0, -1, 0],  # b > c
-    ]
-    b = [
-        3 * c,  # success
-        -2 * c - eps,  # fail
-        2 * c,  # success
-        -2 * c - eps,  # fail
-        -1 * c - eps,  # fail
-        c,  # success
-        -eps,  # fail
-        -c - eps,  # fail
-        0,  # success
-        0,  # success
-        -eps,
-        -c - eps,
-    ]
+    A = []
+    b = []
 
-    f = [0, 0, 1]  # minimize K
+    A.append([-3.0, -3, 1])
+    b.append(3 * c)  # success [3, 3, 3]
+    A.append([3.0, 3, -1])
+    b.append(-2 * c - eps)  # fail [3, 3, 2]
+
+    A.append([-4.0, -3, 1])
+    b.append(2 * c)  # success [3, 3, 2, 1]
+    A.append([4.0, 2, -1])
+    b.append(-2 * c - eps)  # fail [3, 3, 1, 1]
+
+    A.append([-4.0, -3, 1])
+    b.append(c)  # success [3, 2, 2, 1]
+    A.append([-4.0, -3, 1])
+    b.append(0.0)  # success [2, 2, 2, 1]
+    A.append([3.0, 3, -1])
+    b.append(-eps)  # fail [2, 2, 2]
+    A.append([4.0, 2, -1])
+    b.append(-eps)  # fail [2, 2, 1, 1]
+
+    A.append([-5.0, -2, 1])
+    b.append(0)  # success [2, 2, 1, 1, 1]
+    A.append([5.0, 1, -1])
+    b.append(-eps)  # fail [2, 1, 1, 1, 1]
+    A.append([6.0, 0, -1])
+    b.append(-eps)  # fail [1, 1, 1, 1, 1, 1]
+
+    A.append([-7.0, 0, 1])
+    b.append(0)  # success [1, 1, 1, 1, 1, 1, 1]
+
+    A.append([-1.0, 1, 0]), b.append(0)  # a >= b
+    A.append([0.0, -1, 0]), b.append(-c)  # b >= c
+
+    f = [1, 1, 1]  # minimize K
     return A, b, f
 
 
@@ -88,21 +63,26 @@ if __name__ == "__main__":
 
     n_complexity = 3
     dim = 2
-    K_min = n_complexity * dim
-    print('minimum K:', K_min)
 
-    #A, b, f = attempt_one()
-    A, b_vec, f = attempt_two()
+    # TODO: the results depends on the value of the slack variable epsilon.
+    # it shouldn't....
+    eps = 0.1
 
-    equality_matrix = [[1, 1, 0]]  # a + b = 2
-    equality_vector = [2]
+    # TODO chosen like this so that the resulting values for a, b == 1.0.
+    c = 0.45
 
+    A, b_vec, f = setup_constraints(c, eps)
+
+    #equality_matrix = [[1, 1, 0]]  # a + b = 2
+    #equality_vector = [2]
     #res = linprog(f, A_ub=A, b_ub=b_vec, A_eq=equality_matrix,
     #              b_eq=equality_vector)
+
     res = linprog(f, A_ub=A, b_ub=b_vec)
     if not res.success:
-        print('no solution found.')
-        sys.exit
+        print('ERROR: no solution found!')
+
+    print(res)
 
     a, b, K = res.x
     print('a={:2.2f}, b={:2.2f}, c={:2.2f}, K={:2.2f}'.format(a, b, c, K))
