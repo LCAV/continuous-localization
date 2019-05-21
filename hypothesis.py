1dfbb913d8ca0ebc8195306938ccbe0f3f190bed
Michalina Pacholska <micha7a@gmail.com>
2019-05-21 19:46:36 +0200

    Update rank experiments to current theory
    Remove old probability estimates, and new ones
    and add plots comparing new estimates with experiments.

--- hypothesis.py
+++ hypothesis.py
@@ -1,5 +1,5 @@
 import numpy as np
-import os
+import scipy.special as special
 
 
 def get_anchors(n_anchors, n_dimensions=2, scale=10):
@@ -12,23 +12,24 @@ def get_frame(n_constrains, n_positions):
     return np.cos(Ks @ Ns.T * np.pi / n_positions)
 
 
-def get_full_constrains(idx_a, idx_f, anchors, frame):
-    vectors = [np.append(anchors[:, a], frame[:, f]) for (a, f) in zip(idx_a, idx_f)]
-    matrices = [(v[np.newaxis, :].T @ v[np.newaxis, :]).flatten() for v in vectors]
+def get_left_submatrix(idx_a, idx_f, anchors, frame):
+    f_vect = [frame[:, i] for i in idx_f]
+    a_extended = [np.concatenate([anchors[:, a], [1]]) for a in idx_a]
+    matrices = [(a[:, None] @ f[None, :]).flatten() for (f, a) in zip(f_vect, a_extended)]
     return np.array(matrices).T
 
 
-def get_upper_right_constrains(idx_a, idx_f, anchors, frame):
-    matrices = [(anchors[:, a:a + 1] @ frame[:, f:f + 1].T).flatten()
-                for (a, f) in zip(idx_a, idx_f)]
-    return np.array(matrices).T
+def get_reduced_right_submatrix(idx_f, frame):
+    vectors = frame[:, idx_f]
+    matrix = [f[1:] * f[-1] for f in vectors]
+    return np.array(matrix).T
 
 
 def random_indexes(n_anchors, n_positions, n_measurements):
     assert n_positions * n_anchors >= n_measurements, "to many measurements requested"
     indexes = np.random.choice(n_positions * n_anchors, n_measurements, replace=False)
     idx_a, idx_f = np.unravel_index(indexes, (n_anchors, n_positions))
-    return (idx_a.tolist(), idx_f.tolist())
+    return idx_a.tolist(), idx_f.tolist()
 
 
 def indexes_to_matrix(idx_a, idx_f, n_anchors, n_positions):
@@ -39,43 +40,20 @@ def indexes_to_matrix(idx_a, idx_f, n_anchors, n_positions):
 
 def matrix_to_indexes(matrix):
     indexes = np.argwhere(matrix)
-    return (indexes[:, 0].tolist(), indexes[:, 1].tolist())
+    return indexes[:, 0].tolist(), indexes[:, 1].tolist()
 
 
-if __name__ == '__main__':
+def probability_few_anchors(n_dimensions, n_constrains, n_positions):
+    """Calculate probability of size the smallest matrix being invertible with minimal number of anchors.
 
-    n_dimensions = 2
-    n_constrains = 5
-    n_anchors = 10
-    n_positions = 30
-    n_repetitions = 10
-    fixed_probability = True
-    directory = "results/ranks/"
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    Smallest matrix that can invertible has to have (n_dimensions+1)n_constrains rows,
+    and the smallest number of needed anchors is n_dimensions + 1. The behaviour is qualitatively different
+    for n_dimensions + 1 anchors than for more anchors."""
+    total = special.binom((n_dimensions + 1) * n_positions, (n_dimensions + 1) * n_constrains)
+    full = special.binom(n_positions, n_constrains)**(n_dimensions + 1)
+    return full / total
 
-    probabilities = np.linspace(0, 1, 100)
 
-    key = "_d{}_c{}_a{}_p{}_{}".format(n_dimensions, n_constrains, n_anchors, n_positions,
-                                       fixed_probability)
-
-    anchors = get_anchors(n_anchors, n_dimensions)
-    frame = get_frame(n_constrains, n_positions)
-
-    if fixed_probability:
-        ranks = np.zeros((len(probabilities), n_repetitions))
-        for idx, prob in enumerate(probabilities):
-            for r in range(n_repetitions):
-                matrix = np.random.binomial(n=1, p=prob, size=(n_anchors, n_positions))
-                idx_a, idx_f = matrix_to_indexes(matrix)
-                constrains = get_upper_right_constrains(idx_a, idx_f, anchors, frame)
-                ranks[idx, r] = np.linalg.matrix_rank(constrains)
-    else:
-        ranks = np.zeros((n_positions * n_anchors + 1, n_repetitions))
-        for n_measurements in range(2, n_positions * n_anchors + 1):
-            for r in range(n_repetitions):
-                idx_a, idx_f = random_indexes(n_anchors, n_positions, n_measurements)
-                constrains = get_upper_right_constrains(idx_a, idx_f, anchors, frame)
-                ranks[n_measurements, r] = np.linalg.matrix_rank(constrains)
-
-    np.save(directory + "ranks" + key, ranks)
+def probability_few_anchors_limit(n_dimensions, n_constrains):
+    """Calculate analytical limit of probability_few_anchors"""
+    return np.sqrt(n_dimensions + 1) / (np.sqrt(2 * np.pi * n_constrains)**n_dimensions)
