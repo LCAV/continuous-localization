15fd4d8360f174bb4d00079e28a1f4651a33096e
Michalina Pacholska <micha7a@gmail.com>
2019-08-29 19:33:54 +0200

    Change simulations to one measurements per time

--- generate_noise_data.py
+++ generate_noise_data.py
@@ -4,7 +4,7 @@ from multiprocessing import Pool
 
 def f(params):
     outfolder = 'results/{}/'.format(params['key'])
-    run_simulation(params, outfolder, solver=params['solver'])
+    run_simulation(params, outfolder, solver=params['solver'], verbose=True)
 
 
 noise_simgas = [1e-1, 2e-1, 5e-1, 1e0, 2e0, 5e0, 1e1]
@@ -19,7 +19,8 @@ parameters = [{
     'noise_sigmas': noise_simgas,
     'success_thresholds': [0.0] * len(noise_simgas),
     'noise_to_square': True,
-    'solver': "alternativePseudoInverse"
+    'solver': 'alternativePseudoInverse',
+    'sampling_strategy': 'single_time'
 }, {
     'key': 'noise_right_inverse_weighted_2',
     'n_its': 10,
@@ -30,7 +31,8 @@ parameters = [{
     'noise_sigmas': noise_simgas,
     'success_thresholds': [0.0] * len(noise_simgas),
     "noise_to_square": False,
-    'solver': 'weightedPseudoInverse'
+    'solver': 'weightedPseudoInverse',
+    'sampling_strategy': 'single_time'
 }, {
     'key': 'noise_right_inverse_2',
     'n_its': 10,
@@ -41,7 +43,8 @@ parameters = [{
     'noise_sigmas': noise_simgas,
     'success_thresholds': [0.0] * len(noise_simgas),
     "noise_to_square": False,
-    'solver': 'alternativePseudoInverse'
+    'solver': 'alternativePseudoInverse',
+    'sampling_strategy': 'single_time'
 }]
 
 with Pool(3) as p:
