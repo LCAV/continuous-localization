ce7cf0e3a2cd3d83e9e9635d74212aa2f3f5e618
duembgen <frederike.duembgen@epfl.ch>
2019-11-04 18:31:37 +0100

    Fixed latex issue.

--- plotting_tools.py
+++ plotting_tools.py
@@ -19,8 +19,11 @@ def make_dirs_safe(path):
 
 def savefig(fig, name):
     make_dirs_safe(name)
-    fig.savefig(name, bbox_inches='tight', pad_inches=0)
-    print('saved as', name)
+    try:
+        fig.savefig(name, bbox_inches='tight', pad_inches=0)
+        print('saved as', name)
+    except:
+        print('could not save figure under {} for some reason.'.format(name))
 
 
 def add_scalebar(ax, size=5, size_vertical=1, loc='lower left'):
