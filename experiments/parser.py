e57546d9e5a69cbf268d0e7d77cc86b41b982003
duembgen <frederike.duembgen@epfl.ch>
2019-11-19 13:51:02 +0100

    Improved Decawave parsing and added notebook.

--- experiments/parser.py
+++ experiments/parser.py
@@ -18,9 +18,14 @@ def process_line(result_df, line):
     data = line.split('distances: ')[-1]
     meas_list = data.split('}, ')
     for arr in meas_list:
-        anchor_id, length, *_ = arr.split(' ')
-        d = length.split('distance=Distance{length=')[1][:-1]
-        result_df.loc[len(result_df)] = dict(time=time, device_id=device_id, anchor_id=anchor_id, distance=d)
+        anchor_id, length, qual, *_ = arr.split(' ')
+        try:
+            d = float(length.strip('distance=Distance{length=').strip()[:-1]) / 1000.  # remove trailing comma.
+            q = qual.strip('quality=').strip().strip('}')
+        except Exception as e:
+            print('error parsing', length.split('distance=Distance{length='))
+            raise
+        result_df.loc[len(result_df)] = dict(time=time, device_id=device_id, anchor_id=anchor_id, distance=d, quality=q)
 
 
 def anchor_line(anchor_df, line):
@@ -38,7 +43,7 @@ def anchor_line(anchor_df, line):
 
 
 def parse_file(logfile):
-    result_df = pd.DataFrame(columns=['time', 'device_id', 'anchor_id', 'distance'])
+    result_df = pd.DataFrame(columns=['time', 'device_id', 'anchor_id', 'distance', 'quality'])
     anchor_df = pd.DataFrame(columns=['anchor_id', 'px', 'py', 'pz', 'full_id'])
     with open(logfile, 'r') as f:
         counter = 0
@@ -55,21 +60,29 @@ def parse_file(logfile):
         while line:
             line = f.readline()
             process_line(result_df, line)
+
+    result_df = result_df.apply(pd.to_numeric, errors='ignore', axis=0)
+    anchor_df = anchor_df.apply(pd.to_numeric, errors='ignore', axis=0)
     return result_df, anchor_df
 
 
 if __name__ == "__main__":
-    logfile = 'UWB/monday.txt'
-    print('processing', logfile)
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description='Parse decawave logfile.')
+    parser.add_argument('filenames', metavar='fname', type=str, nargs='+', help='filenames to process')
+    args = parser.parse_args()
+    logfiles = args.filenames  #'UWB/monday.txt'
+    for logfile in logfiles:
+        print('processing', logfile)
 
-    result_df, anchor_df = parse_file(logfile)
-    print('result_df')
-    print(result_df.head())
+        result_df, anchor_df = parse_file(logfile)
+        print('result_df')
+        print(result_df.head())
 
-    resultfile = logfile.replace('.txt', '.pkl')
-    result_df.to_pickle(resultfile)
-    print('saved as', resultfile)
+        resultfile = logfile.replace('.txt', '.pkl')
+        result_df.to_pickle(resultfile)
+        print('saved as', resultfile)
 
-    resultfile = logfile.replace('.txt', '_anchors.pkl')
-    anchor_df.to_pickle(resultfile)
-    print('saved as', resultfile)
+        resultfile = logfile.replace('.txt', '_anchors.pkl')
+        anchor_df.to_pickle(resultfile)
+        print('saved as', resultfile)
