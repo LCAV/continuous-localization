5613f738b1dc92044aa3033561385b871e9c0ff5
duembgen <frederike.duembgen@epfl.ch>
2019-11-25 17:10:34 +0100

    Started with generation of polynomial results.

--- generate_results.py
+++ generate_results.py
@@ -8,138 +8,85 @@ import pandas as pd
 from scipy.io import loadmat
 import seaborn as sns
 
-from public_data_utils import read_dataset, get_ground_truth, get_plotting_params
-from evaluate_dataset import compute_distance_matrix, compute_anchors
 from fit_curve import fit_trajectory
 from other_algorithms import apply_algorithm, error_measure
-from plotting_tools import plot_complexities, add_scalebar
+
+METHODS = ['ours-weighted', 'ours', 'lm-ellipse', 'lm-ours-weighted', 'srls', 'rls']
+
+
+def generate_suitable_mask(D, dim, K, n_measurements):
+    counter = 0
+    while counter < 100:
+        indices = sorted(np.random.choice(D.shape[0], n_measurements, replace=False))
+        D_small = D[indices, :]
+        if test_hypothesis(D_small, dim, K):
+            return indices
+        counter += 1
+    raise ValueError('Did not find suitable mask after 100.')
 
 
 def test_hypothesis(D, dim, K):
     import hypothesis as h
     mask = (D)
     p = np.sort(np.sum(mask, axis=0))[::-1]
-    assert h.limit_condition(list(p), dim + 1, K)
+    return h.limit_condition(list(p), dim + 1, K)
 
 
-def add_measurement(result_df, method=''):
-    result_df.loc[len(result_df)] = dict(n_complexity=n_complexity,
-                                         n_measurements=n_measurements,
-                                         method=method,
-                                         n_it=n_it,
-                                         mae=mae,
-                                         mse=mse)
+def generate_results(traj, D_small, times_small, anchors, points_small, methods=METHODS, n_it=0):
+    n_complexity = traj.n_complexity
+    n_measurements = np.sum(D_small > 0)
+    current_results = pd.DataFrame(
+        columns=['n_it', 'n_complexity', 'n_measurements', 'mae', 'mse', 'method', 'plotting'])
+
+    basis_small = traj.get_basis(times=times_small)
+
+    for method in methods:
+        C_hat, p_hat, lat_idx = apply_algorithm(traj, D_small, times_small, anchors, method=method)
+        plotting = (C_hat, p_hat)
+        traj.set_coeffs(coeffs=C_hat)
+        p_fitted = traj.get_sampling_points(times=times_small).T
+        mae = error_measure(p_fitted, points_small, 'mae')
+        mse = error_measure(p_fitted, points_small, 'mse')
+        current_results.loc[len(current_results)] = dict(plotting=plotting,
+                                                         n_complexity=n_complexity,
+                                                         n_measurements=n_measurements,
+                                                         method=method,
+                                                         n_it=n_it,
+                                                         mae=mae,
+                                                         mse=mse)
+
+        # do raw version if applicable
+        if method in ['rls', 'srls']:
+            points_small_lat = points_small[lat_idx]
+            mae = error_measure(p_hat, points_small_lat, 'mae')
+            mse = error_measure(p_hat, points_small_lat, 'mse')
+            current_results.loc[len(current_results)] = dict(plotting=(None, None),
+                                                             n_complexity=n_complexity,
+                                                             n_measurements=n_measurements,
+                                                             method=method + ' raw',
+                                                             n_it=n_it,
+                                                             mae=mae,
+                                                             mse=mse)
+    return current_results
 
 
-if __name__ == "__main__":
-    ##### Initialization  #####
-    np.random.seed(1)
+def add_gt_fitting(traj, times_small, points_small, current_results, n_it=0):
+    # fit ground truth to chosen points.
+    n_complexity = traj.n_complexity
+    n_measurements = len(times_small)
 
-    #filename = 'datasets/uah1.mat' # fingers
-    #filename = 'datasets/Plaza1.mat'; # zig zag.
-    filename = 'datasets/Plaza2.mat'  # triangle
-    #filename = 'datasets/Gesling1.mat'  #
-    #filename = 'datasets/Gesling2.mat'  #
+    coeffs = fit_trajectory(points_small.T, times=times_small, traj=traj)
+    traj_gt = traj.copy()
+    traj_gt.set_coeffs(coeffs=coeffs)
+    points_fitted = traj_gt.get_sampling_points(times=times_small).T
 
-    resultname = 'results/algorithms_tuesday.pkl'
-
-    full_df, anchors_df, traj = read_dataset(filename)
-    xlim, ylim = get_plotting_params(filename)
-
-    chosen_distance = 'distance'
-    #chosen_distance = 'distance_gt'
-    range_system_id = 'Range'
-    assert range_system_id in full_df.system_id.unique()
-
-    list_complexities = [3, 5, 11, 19]
-    list_measurements = [40, 100, 200, 300, 400, 499]
-    methods = ['ours-weighted', 'ours']
-    methods += ['lm-ellipse', 'lm-ours-weighted']
-    methods += ['srls', 'rls']
-    total_n_it = 20
-    anchor_names = None  # use all anchors.
-
-    plotting = True
-    verbose = True
-
-    ##### Bring data in correct form #####
-    anchors = compute_anchors(anchors_df, anchor_names)
-    times = full_df[full_df.system_id == range_system_id].timestamp.unique()
-    D, times = compute_distance_matrix(full_df, anchors_df, anchor_names, times, chosen_distance)
-    if np.sum(D > 0) > D.shape[0]:
-        print('Warning: multiple measurements for times:{}/{}!'.format(np.sum(np.sum(D > 0, axis=1) > 1), D.shape[0]))
-    points_gt = get_ground_truth(full_df, times).values
-    anchors = anchors[:2, :]
-
-    ##### Run experiments #####
-    if plotting:
-        fig, axs = plt.subplots(len(list_measurements), len(list_complexities), sharex=True, sharey=True)
-        fig_size = [5, 1.2 * len(list_measurements)]
-
-    result_df = pd.DataFrame(columns=['n_it', 'n_complexity', 'n_measurements', 'mae', 'mse', 'method'])
-    for j, n_complexity in enumerate(list_complexities):
-        if verbose:
-            print(f'K={n_complexity}')
-        traj.set_n_complexity(n_complexity)
-
-        if plotting:
-            axs[0, j].set_title(f'K={n_complexity}')
-
-        for i, n_measurements in enumerate(list_measurements):
-            if plotting:
-                axs[i, 0].set_ylabel(f'N={n_measurements}')
-            if verbose:
-                print(f'n_measurements={n_measurements}')
-
-            for n_it in range(total_n_it):
-                indices = sorted(np.random.choice(D.shape[0], n_measurements, replace=False))
-                D_small = D[indices, :]
-
-                # test hypothesis
-                test_hypothesis(D_small, traj.dim, traj.n_complexity)
-
-                times_small = np.array(times)[indices]
-                basis_small = traj.get_basis(times=times_small)
-                points_small = points_gt[indices, :]
-
-                results = {}
-                for method in methods:
-                    C_hat, p_hat, lat_idx = apply_algorithm(traj, D_small, times_small, anchors, method=method)
-                    results[method] = (C_hat, p_hat)
-                    traj.set_coeffs(coeffs=C_hat)
-                    p_fitted = traj.get_sampling_points(times=times_small).T
-                    mae = error_measure(p_fitted, points_small, 'mae')
-                    mse = error_measure(p_fitted, points_small, 'mse')
-                    add_measurement(result_df, method=method)
-
-                    # do raw version if applicable
-                    if method in ['rls', 'srls']:
-                        points_lat = points_small[lat_idx]
-                        mae = error_measure(p_hat, points_lat, 'mae')
-                        mse = error_measure(p_hat, points_lat, 'mse')
-                        add_measurement(result_df, method=method + ' raw')
-                # fit ground truth to chosen points.
-                coeffs = fit_trajectory(points_small.T, times=times_small, traj=traj)
-                traj_gt = traj.copy()
-                traj_gt.set_coeffs(coeffs=coeffs)
-                points_fitted = traj_gt.get_sampling_points(times=times_small).T
-
-                mse = error_measure(points_fitted, points_small, 'mse')
-                mae = error_measure(points_fitted, points_small, 'mae')
-                add_measurement(result_df, 'gt')
-
-            if resultname != '':
-                result_df.to_pickle(resultname)
-                print('saved as', resultname)
-
-            if plotting:
-                ax = axs[i, j]
-                ax = plot_complexities(traj, times_small, results, points_fitted, ax)
-
-    if plotting:
-        fig.set_size_inches(*fig_size)
-        add_scalebar(axs[0, 0], 20, loc='lower left')
-        [ax.set_xlim(*xlim) for ax in axs.flatten()]
-        [ax.set_ylim(*ylim) for ax in axs.flatten()]
-        plt.show()
-    print('done')
+    mse = error_measure(points_fitted, points_small, 'mse')
+    mae = error_measure(points_fitted, points_small, 'mae')
+    current_results.loc[len(current_results)] = dict(plotting=(coeffs, points_fitted),
+                                                     n_complexity=n_complexity,
+                                                     n_measurements=n_measurements,
+                                                     method='gt',
+                                                     n_it=n_it,
+                                                     mae=mae,
+                                                     mse=mse)
+    return points_fitted
